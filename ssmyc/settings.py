5783cb33bef8767737634b719b18bd153a2aeebd
Yizzy <yizzy@nlplab.cc>
2020-10-20 11:51:00 +0000

    fixed: add STATIC_ROOT

--- ssmyc/settings.py
+++ ssmyc/settings.py
@@ -123,5 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_URL
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
